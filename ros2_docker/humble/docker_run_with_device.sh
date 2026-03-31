b60261f39eb017d707c2f908d2c315c111922dbe
Yutaro Kato <120556494+Yutarop@users.noreply.github.com>
2026-03-31 19:53:24 +0900

    Update docker_run_with_device.sh

---
+++ ros2_docker/humble/docker_run_with_device.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+   echo "Error: You must specify the container name as the first argument."
+   echo "Usage: $0 <container_name>"
+   exit 1
+fi
+
+CONTAINER_NAME=$1
+
+read -sp "Enter password for user 'ubuntu': " PASSWORD
+echo
+
+
+sudo docker run \
+   --name "$CONTAINER_NAME" \
+   --ipc host \
+   --net host \
+   --shm-size=512m \
+   --security-opt seccomp=unconfined \
+   --device "/dev/input/js0:/dev/input/js0" \
+   --device /dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0:/dev/ZLAC8015D \
+   --env USER=ubuntu \
+   --env PASSWORD="$PASSWORD" \
+   yutarop/my-ros2-desktop:humble

8594bbca1312d9615dbadaab7fe1156013418789
Yutaro Kato <120556494+Yutarop@users.noreply.github.com>
2026-04-03 15:14:03 +0900

    Add video device support to docker run script

--- ros2_docker/humble/docker_run_with_device.sh
+++ ros2_docker/humble/docker_run_with_device.sh
@@ -19,7 +19,8 @@ sudo docker run \
    --shm-size=512m \
    --security-opt seccomp=unconfined \
    --device "/dev/input/js0:/dev/input/js0" \
-   --device /dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0:/dev/ZLAC8015D \
+   --device "/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0:/dev/ZLAC8015D" \
+   --device "/dev/v4l/by-id/usb-046d_0821_A3461BA0-video-index0:/dev/video0" \
    --env USER=ubuntu \
    --env PASSWORD="$PASSWORD" \
    yutarop/my-ros2-desktop:humble
